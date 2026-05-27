48ef3e774297aa48637bd3591423306baec8f7dd
yutakobayashidev <hi@yutakobayashi.com>
2026-05-27 11:10:12 +0900

    feat(navidrome): enable transcoding for browser playback

--- systems/nixos/services/navidrome/default.nix
+++ systems/nixos/services/navidrome/default.nix
@@ -1,12 +1,16 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   services.navidrome = {
     enable = true;
     settings = {
       MusicFolder = "/srv/bulk/music";
       EnableSharing = true;
+      EnableTranscodingConfig = true;
+      TranscodingCacheSize = "100MiB";
     };
   };
+
+  systemd.services.navidrome.path = [ pkgs.ffmpeg ];
 
   systemd.tmpfiles.rules = [
     "d /srv/bulk/music 2775 yuta users - -"
