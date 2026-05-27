b95cbb4c38a1e80636f201da730be356c78622c7
yutakobayashidev <hi@yutakobayashi.com>
2026-05-27 09:57:35 +0900

    refactor(beets): use pkgs.formats.yaml.generate for declarative config

--- applications/beets/default.nix
+++ applications/beets/default.nix
@@ -1,28 +1,35 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
+let
+  format = pkgs.formats.yaml { };
+  beetsConf = {
+    directory = "/srv/bulk/music";
+    library = "/home/yuta/.local/share/beets/library.db";
+    import = {
+      copy = false;
+      move = true;
+      write = true;
+      resume = "ask";
+      incremental = true;
+      quiet_fallback = "skip";
+      timid = false;
+      log = null;
+    };
+    paths = {
+      default = "\${albumartist}/\${album}/\${track} \${title}";
+      singleton = "Non-Album/\${artist}/\${title}";
+      comp = "Compilations/\${album}/\${track} \${title}";
+    };
+    ui.color = true;
+    plugins = "fetchart embedart scrub replaygain";
+    replaygain = {
+      backend = "ffmpeg";
+      auto = true;
+    };
+  };
+in
 {
   home.packages = with pkgs; [ beets ];
 
-  xdg.configFile."beets/config.yaml".text = ''
-    directory: /srv/bulk/music
-    library: /home/yuta/.local/share/beets/library.db
-    import:
-      copy: no
-      move: yes
-      write: yes
-      resume: ask
-      incremental: yes
-      quiet_fallback: skip
-      timid: no
-      log:
-    paths:
-      default: ''${albumartist}/''${album}/''${track} ''${title}
-      singleton: Non-Album/''${artist}/''${title}
-      comp: Compilations/''${album}/''${track} ''${title}
-    ui:
-      color: yes
-    plugins: fetchart embedart scrub replaygain
-    replaygain:
-      backend: ffmpeg
-      auto: yes
-  '';
+  xdg.configFile."beets/config.yaml".source =
+    format.generate "beets-config.yaml" beetsConf;
 }
