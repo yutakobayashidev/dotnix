cf734dc910beb86295875b2eaab975aab76e18ff
yutakobayashidev <hi@yutakobayashi.com>
2026-05-27 09:53:21 +0900

    refactor(whipper): use lib.generators.toINI for declarative config

--- applications/whipper/default.nix
+++ applications/whipper/default.nix
@@ -1,15 +1,18 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let
+  whipperConf = {
+    "whipper.cd.rip" = {
+      output_directory = "/srv/bulk/music/_inbox";
+      unknown = true;
+      cdr = true;
+      cover_art = "complete";
+    };
+    musicbrainz.https = true;
+  };
+in
 {
   home.packages = [ pkgs.whipper ];
 
-  xdg.configFile."whipper/whipper.conf".text = ''
-    [whipper.cd.rip]
-    output_directory = /srv/bulk/music/_inbox
-    unknown = True
-    cdr = True
-    cover_art = complete
-
-    [musicbrainz]
-    https = True
-  '';
+  xdg.configFile."whipper/whipper.conf".text =
+    lib.generators.toINI { } whipperConf;
 }
