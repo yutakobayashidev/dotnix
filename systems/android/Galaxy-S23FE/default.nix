89ab79fc5eac4e0fd8ac6f868448fcfe0e7e45ed
yutakobayashidev <hi@yutakobayashi.com>
2026-06-01 08:16:06 +0900

    feat: add nix-on-droid system config for Galaxy S23FE

--- systems/android/Galaxy-S23FE/default.nix
+++ systems/android/Galaxy-S23FE/default.nix
@@ -1,5 +1,23 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [ ../../../nix/modules/nix-on-droid ];
+
+  system.stateVersion = "24.05";
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+  environment = {
+    etcBackupExtension = ".bak";
+    etc."resolv.conf".source = ./resolv.conf;
+  };
+  android-integration = {
+    am.enable = true;
+    termux-open.enable = true;
+    termux-open-url.enable = true;
+    xdg-open.enable = true;
+  };
+  terminal.font = "${pkgs.firge-nerd-font}/share/fonts/firge-nerd/FirgeNerdConsole-Regular.ttf";
+  time.timeZone = "Asia/Tokyo";
+  user.shell = "${pkgs.zsh}/bin/zsh";
 }
