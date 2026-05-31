26bf20933b32c762b8dbb02710b8632dfccc0ffa
yutakobayashidev <hi@yutakobayashi.com>
2026-06-01 08:12:40 +0900

    chore: remove non-home-manager settings from nix-on-droid module (moved to nur-packages/systems/android/Galaxy-S23FE)

--- nix/modules/nix-on-droid/default.nix
+++ nix/modules/nix-on-droid/default.nix
@@ -1,17 +1,6 @@
 { pkgs, ... }:
 
 {
-  environment.packages = with pkgs; [
-    git
-    openssh
-  ];
-
-  system.stateVersion = "24.05";
-
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes
-  '';
-
   home-manager = {
     backupFileExtension = "hm-bak";
     useGlobalPkgs = true;
