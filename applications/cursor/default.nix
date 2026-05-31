2b865e1b7a17f795f5265dc14310dc7c3492d692
yutakobayashidev <hi@yutakobayashi.com>
2026-06-01 06:44:12 +0900

    revert: restore derivation-based vscode-file-nesting-config (eval-only CI no longer triggers the build)

--- applications/cursor/default.nix
+++ applications/cursor/default.nix
@@ -4,7 +4,7 @@ let
   inherit (pkgs) vscode-extensions vscode-utils;
 
   fileNestingSettings = builtins.fromJSON (
-    builtins.readFile ./vscode-file-nesting-config/config.json
+    builtins.readFile (pkgs.callPackage ./vscode-file-nesting-config { })
   );
 
   marketplaceExtensions = vscode-utils.extensionsFromVscodeMarketplace [
