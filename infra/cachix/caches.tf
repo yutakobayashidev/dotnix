931783805ae96f7970ad15619ac55fe8714bca8a
yutakobayashidev <hi@yutakobayashi.com>
2026-06-01 07:26:43 +0900

    infra: remove yutakobayashidev-nur cachix cache

--- infra/cachix/caches.tf
+++ infra/cachix/caches.tf
@@ -2,8 +2,3 @@ resource "cachix_cache" "yuta" {
   name      = "yuta"
   is_public = true
 }
-
-resource "cachix_cache" "yutakobayashidev_nur" {
-  name      = "yutakobayashidev-nur"
-  is_public = true
-}
