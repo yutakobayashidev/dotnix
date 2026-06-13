0804cbbc5a45c429873ad531ad6b35716fda24af
bot-rucachan <nakasyou1103+ruca@gmail.com>
2026-06-13 15:18:03 +0900

    chore(gitea): add nakasyou user (#878)
    Co-authored-by: Shotaro Nakamura <syoutarou1103@outlook.jp>

--- infra/gitea/users.tf
+++ infra/gitea/users.tf
@@ -8,19 +8,32 @@ resource "random_password" "akazdayo" {
   special = true
 }
 
+resource "random_password" "nakasyou" {
+  length  = 24
+  special = true
+}
+
 resource "gitea_user" "moons_14" {
-  username            = "moons-14"
-  login_name          = "moons-14"
-  email               = "moons@moons14.com"
-  password            = random_password.moons_14.result
+  username             = "moons-14"
+  login_name           = "moons-14"
+  email                = "moons@moons14.com"
+  password             = random_password.moons_14.result
   must_change_password = true
 }
 
 resource "gitea_user" "akazdayo" {
-  username            = "akazdayo"
-  login_name          = "akazdayo"
-  email               = "me@odango.app"
-  password            = random_password.akazdayo.result
+  username             = "akazdayo"
+  login_name           = "akazdayo"
+  email                = "me@odango.app"
+  password             = random_password.akazdayo.result
+  must_change_password = true
+}
+
+resource "gitea_user" "nakasyou" {
+  username             = "nakasyou"
+  login_name           = "nakasyou"
+  email                = "how@nakasyou.how"
+  password             = random_password.nakasyou.result
   must_change_password = true
 }
 
@@ -31,5 +44,10 @@ output "moons_14_password" {
 
 output "akazdayo_password" {
   value     = random_password.akazdayo.result
+  sensitive = true
+}
+
+output "nakasyou_password" {
+  value     = random_password.nakasyou.result
   sensitive = true
 }
