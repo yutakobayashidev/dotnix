7bbf36713fd768fd0c70b4dc68b06133c0d50c9b
yutakobayashidev <hi@yutakobayashi.com>
2026-06-17 15:58:28 +0900

    feat(gitea-actions-runner): use default instance, hostName, add nix:host label with hostPackages

--- systems/nixos/services/gitea-actions-runner/default.nix
+++ systems/nixos/services/gitea-actions-runner/default.nix
@@ -1,20 +1,37 @@
-{ config, ... }:
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
 
 let
   domain = "git.yutakobayashi.com";
 in
 {
-  services.gitea-actions-runner.instances.B450M-Pro4 = {
+  services.gitea-actions-runner.instances.default = {
     enable = true;
-    name = "B450M-Pro4";
+    name = config.networking.hostName;
     url = "https://${domain}";
     tokenFile = config.sops.secrets.gitea-actions-runner-token.path;
     labels = [
       "ubuntu-24.04:docker://ghcr.io/gitea/runner-images:ubuntu-24.04"
+      "nix:host"
+    ];
+    hostPackages = with pkgs; [
+      bash
+      busybox
+      curl
+      git
+      nodejs
+      nix
+      inputs.niks3.packages.${pkgs.stdenv.hostPlatform.system}.niks3
     ];
   };
 
   sops.secrets.gitea-actions-runner-token = {
     sopsFile = ./secrets.yaml;
   };
+
+  virtualisation.docker.enable = true;
 }
