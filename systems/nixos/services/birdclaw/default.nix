183892cb42736d2693a94f9c43c744e8034408db
yutakobayashidev <hi@yutakobayashi.com>
2026-06-23 23:05:08 +0900

    feat: expose birdclaw via traefik

--- systems/nixos/services/birdclaw/default.nix
+++ systems/nixos/services/birdclaw/default.nix
@@ -1,5 +1,10 @@
 { inputs, ... }:
 
+let
+  domain = "birdclaw.home.yutakobayashi.com";
+  port = 3005;
+in
+
 {
   imports = [
     inputs.nur-packages.nixosModules.birdclaw
@@ -7,6 +12,22 @@
 
   services.birdclaw = {
     enable = true;
-    port = 3005;
+    host = "127.0.0.1";
+    inherit port;
+  };
+
+  services.traefik.dynamicConfigOptions.http = {
+    routers.birdclaw = {
+      entryPoints = [
+        "web"
+        "websecure"
+      ];
+      rule = "Host(`${domain}`)";
+      service = "birdclaw";
+      tls.certResolver = "letsencrypt";
+    };
+    services.birdclaw.loadBalancer.servers = [
+      { url = "http://127.0.0.1:${toString port}"; }
+    ];
   };
 }
