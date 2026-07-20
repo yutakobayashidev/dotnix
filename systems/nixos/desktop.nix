fa3012be97329ddaf039e1ab6704a73bca9785d8
yutakobayashidev <hi@yutakobayashi.com>
2026-07-21 08:33:11 +0900

    fix(niri): remove uwsm session

--- systems/nixos/desktop.nix
+++ systems/nixos/desktop.nix
@@ -12,14 +12,6 @@
     gnome-disks.enable = true;
     niri.enable = true;
     obs-studio.enableVirtualCamera = true;
-    uwsm = {
-      enable = true;
-      waylandCompositors.niri = {
-        prettyName = "Niri";
-        comment = "A scrollable-tiling Wayland compositor managed by UWSM";
-        binPath = "/run/current-system/sw/bin/niri";
-      };
-    };
     xwayland.enable = true;
     regreet = {
       enable = true;
