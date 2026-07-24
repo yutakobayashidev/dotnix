c89d4d7c3dc5d93d6786f9ccc4fb81209a90a445
yutakobayashidev <hi@yutakobayashi.com>
2026-07-24 17:28:23 +0900

    fix(niri): geometry-corner-radius

--- applications/niri/default.nix
+++ applications/niri/default.nix
@@ -81,7 +81,12 @@
     # ウィンドウルール
     window-rules = [
       {
-        geometry-corner-radius = 10.0;
+        geometry-corner-radius = {
+          top-left = 10.0;
+          top-right = 10.0;
+          bottom-right = 10.0;
+          bottom-left = 10.0;
+        };
         clip-to-geometry = true;
       }
 
